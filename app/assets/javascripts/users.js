c95a5d24353c4435490e08c68dfaf4907abe67d0
Alex A <alexaa0088@gmail.com>
2015-08-24 16:10:51 +0000

    Updated stripe configs

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -1,3 +1,4 @@
+
 $(document).ready(function() {
   Stripe.setPublishableKey($('meta[name="stripe-key"]').attr('content'));
   // Watch for a form submission:
@@ -9,7 +10,6 @@ $(document).ready(function() {
         cvcNum = $('#card_code').val(),
         expMonth = $('#card_month').val(),
         expYear = $('#card_year').val();
-        
     if (!error) {
       // Get the Stripe token:
       Stripe.createToken({
@@ -21,17 +21,13 @@ $(document).ready(function() {
     }
     return false;
   }); // form submission
-  
   function stripeResponseHandler(status, response) {
     // Get a reference to the form:
     var f = $("#new_user");
-    
     // Get the token from the response:
     var token = response.id;
-    
     // Add the token to the form:
     f.append('<input type="hidden" name="user[stripe_card_token]" value="' + token + '" />');
-    
     // Submit the form:
     f.get(0).submit(); 
   }
